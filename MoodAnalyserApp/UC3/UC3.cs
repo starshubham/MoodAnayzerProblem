5ea092d73c70d68cc92dbc33394a47c761f84630
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-05 14:50:58 +0530

    Added uc3 program with TC3

---
+++ MoodAnalyserApp/UC3/UC3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MoodAnalyzerApp.UC3
+{
+    public class MoodAnalyse3
+    {
+        private string message;
+
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyse3(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.EMPTY_MESSAGE, "Mood should not be Empty");
+                }
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
